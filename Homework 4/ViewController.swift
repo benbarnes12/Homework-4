d3993ebff937be88ccc888a19ed98e35c3aab66c
Ben Barnes <bm.barnes@setonhill.edu>
2020-03-04 19:42:47 -0500

    Initial Commit

---
+++ Homework 4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 4
+//
+//  Created by ben barnes on 3/4/20.
+//  Copyright © 2020 ben barnes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
