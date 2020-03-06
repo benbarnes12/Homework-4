cc5798d3bc9b3713521d26186a0af4f25977ac9b
Ben Barnes <bm.barnes@setonhill.edu>
2020-03-05 19:27:42 -0500

    ready

--- Homework 4/ViewController.swift
+++ Homework 4/ViewController.swift
@@ -10,11 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBAction func Notification1(_ sender: Any) {
+        
+        let alert =
+           
+            UIAlertController(title: "Update", message: " I love programming 2.", preferredStyle: .alert)
+        alert
+        .addAction(UIAlertAction(title:
+        NSLocalizedString("OK",
+        comment: "Default action"), style: .default, handler: { _ in}))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func Notification2(_ sender: Any) {
+        
+        let alert =
+        
+            UIAlertController(title: "Update", message: " Ben Barnes is best.", preferredStyle: .alert)
+        alert
+            .addAction(UIAlertAction( title: NSLocalizedString("Ok", comment: "Defualt action"), style: .default, handler: { _ in}))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func Notification3(_ sender: Any) {
+        let alert =
+                      
+                          UIAlertController(title: "Update", message: " Xcode is great.", preferredStyle: .alert)
+                      alert
+                          .addAction(UIAlertAction( title: NSLocalizedString("Ok", comment: "Defualt action"), style: .default, handler: { _ in}))
+                      self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
